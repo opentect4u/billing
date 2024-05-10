1d3eaea623547d6593bcbdb8512e4b49a03ef67f
rupsachatterjee43 <chatterjeerupsa.43@gmail.com>
2024-05-10 18:34:23 +0530

    Completed Supplier Details-10/05/24

--- module/PurchaseModule.js
+++ module/PurchaseModule.js
@@ -10,6 +10,16 @@ const supplier_lt = (comp_id, keyWord) => {
   });
 };
 
+const sup_list_id = (id) => {
+  return new Promise(async (resolve, reject) => {
+    var select = "*",
+    table_name = "md_supplier",
+    where = `id = ${id}`;
+    var res_dt = await db_Select(select,table_name,where,null);
+    resolve(res_dt);
+  });
+  };
+
 const supplier_list = (comp_id) => {
     return new Promise(async (resolve, reject) => {
       var select = "*",
@@ -19,6 +29,30 @@ const supplier_list = (comp_id) => {
       resolve(res_dt);
     });
   };
+
+const sup_edit_dtls = (id) => {
+    return new Promise(async (resolve, reject) => {
+      var select = "id,comp_id,supplier_name,gstin,address",
+      table_name = "md_supplier",
+      where = `id = ${id}`;
+      var res_dt = await db_Select(select,table_name,where,null);
+      resolve(res_dt);
+    });
+  };
+
+const save_edit_sup_data = (data) =>{
+    return new Promise(async (resolve, reject) => {
+     datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
+     var table_name = "md_supplier",
+     fields = `supplier_name ='${data.supplier_name}', gstin = '${data.gstin}', address = '${data.address}', modified_by = 'admin', modified_at = '${datetime}'`,
+     values = null,
+     where = `id = '${data.id}'`,
+     flag = 1;
+     var res_dt = await db_Insert(table_name,fields,values,where,flag);
+     resolve(res_dt);
+    //  console.log(res_dt,'88');
+    })
+  }  
 
 const save_add_sup_data = (data,comp_id) => {
     return new Promise (async (resolve, reject) =>{
@@ -32,8 +66,48 @@ const save_add_sup_data = (data,comp_id) => {
      resolve(res_dt);
     })
   }
+// ================================================================================================
+const br_list = (comp_id) => {
+  return new Promise(async (resolve, reject) => {
+    var select = "*",
+      table_name = "md_branch",
+      where = `comp_id = '${comp_id}'`;
+    var res_dt = await db_Select(select, table_name, where, null);
+    resolve(res_dt);
+  });
+};
+
+const save_add_purchase_data = (data,comp_id) => {
+  return new Promise (async (resolve, reject) =>{
+   current_datetime = datetime.now()
+   receipt = int(round(current_datetime.timestamp()))
+   datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
+   date = dateFormat(new Date(), "yyyy-mm-dd");
+   var table_name = "td_purchase",
+   fields = `(purchase_id,comp_id,br_id, supplier_id,invoice_no,invoice_date,pay_mode,created_by,created_at)`,
+   values = `('${receipt}','${comp_id}','${data.br_id}','${data.supplier_id}', '${data.invoice_no}','${date}','${data.pay_mode}','admin','${datetime}')`,
+   where = null,
+   flag = 0;
+   var res_dt = await db_Insert(table_name,fields,values,where,flag);
+  //  if(res_dt.suc > 0){
+  //   var table_name1 = "md_item_rate",
+  //  fields1 = `(item_id,price,discount,cgst,sgst,created_by,created_dt)`,
+  //  values1 = `(${res_dt.lastId.insertId},'${data.price}','${data.discount}','${data.cgst}','${data.sgst}','admin','${datetime}')`,
+  //  where1 = null,
+  //  flag1 = 0;
+  //  var res_dt1 = await db_Insert(table_name1,fields1,values1,where1,flag1);
+  //  }
+   resolve(res_dt);
+  })
+}
 
 module.exports = { 
 supplier_lt,
 supplier_list,
-save_add_sup_data };
+save_add_sup_data,
+save_edit_sup_data,
+sup_edit_dtls,
+sup_list_id,
+save_add_purchase_data,
+br_list
+ };
--- routes/PurchaseRouter.js
+++ routes/PurchaseRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const{ supplier_lt, supplier_list, save_add_sup_data } = require("../module/PurchaseModule");
+const{ supplier_lt, supplier_list, save_add_sup_data, save_edit_sup_data, sup_edit_dtls, sup_list_id, save_add_purchase_data, br_list } = require("../module/PurchaseModule");
 const{ getUnitList } = require("../module/UnitModule");
 const PurchaseRouter = express.Router();
 
@@ -28,7 +28,32 @@ PurchaseRouter.get('/supplier_list', async (req, res) => {
     var item_list = await supplier_lt(comp_id, keyword);
     // console.log(item_list);
     res.send(supplier_list)
+  });
+
+PurchaseRouter.get('/sup_name_list', async (req, res) => {
+    var data = req.query;
+    // console.log(data,"1234");
+    var item = await sup_list_id(data.id);
+    // console.log(item);
+    res.send(item)
   })
+
+PurchaseRouter.get('/add_edit_dtls', async (req, res) => {
+    var data = req.query;
+    var comp_id = req.session.user.comp_id;
+    // console.log(data,"lalal");
+    var sup_dtl = await supplier_list(comp_id),
+    unit_dt = await getUnitList(comp_id);
+    var sup_edit_dtl = await sup_edit_dtls(data.id)
+    // console.log(item_dtl);
+    var res_dt = {
+      sup_dt: sup_dtl.suc > 0 ? sup_dtl.msg : [],
+      sup_edit_dt: sup_edit_dtl.suc > 0 ? sup_edit_dtl.msg : [],
+      unit_dt: unit_dt.suc > 0 ? unit_dt.msg : [],
+      id: data.id,
+    }
+    res.render("purchase/edit_sup_dtls", res_dt);
+  });
 
 PurchaseRouter.get("/add_dtls", async (req, res) =>{
     var data = req.query;
@@ -42,6 +67,14 @@ PurchaseRouter.get("/add_dtls", async (req, res) =>{
     res.render("purchase/add_sup_dtls",res_dt);
   });
 
+PurchaseRouter.post('/save_edit_data', async (req, res) => {
+    var data = req.body;
+    // console.log(data,"maaaaaa");
+  var edit_data = await save_edit_sup_data(data);
+  // console.log(edit_data);
+  res.redirect("/purchase/supplier_details")
+  });
+
 PurchaseRouter.post("/save_data", async (req, res) => {
     var data = req.body;
     // console.log(data,"lalal");
@@ -49,5 +82,27 @@ PurchaseRouter.post("/save_data", async (req, res) => {
     var add_data = await save_add_sup_data(data,comp_id);
     res.redirect("/purchase/supplier_details")
   });
+
+  PurchaseRouter.post("/purchase_info", async (req, res) => {
+    res.redirect("/purchase/purchase_info")
+  });
+
+// ==============================================================================================
+PurchaseRouter.get("/brn_list", async (req, res) => {
+  comp_id = req.session.user.comp_id
+  var brn_list = await br_list(comp_id);
+  var res_dt = {
+    data: brn_list.suc > 0 ? brn_list.msg : [],
+  };
+  res.render("purchase/purchase_info", res_dt);
+});
+
+PurchaseRouter.post("/save_purchase_data", async (req, res) => {
+  var data = req.body;
+  // console.log(data,"lalal");
+  comp_id = req.session.user.comp_id
+  var add_data = await save_add_purchase_data(data,comp_id);
+  res.redirect("/purchase/purchase_info")
+});
 
 module.exports = { PurchaseRouter };
